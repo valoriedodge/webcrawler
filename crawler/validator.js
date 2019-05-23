0fa51b1bda9c1dbb098ae98c450c2874958055e1
Nikolas Brix <nsbrix@gmail.com>
2019-05-23 14:48:51 -0700

    Update validator.js

--- crawler/validator.js
+++ crawler/validator.js
@@ -12,31 +12,32 @@ const validUrl = require('valid-url');
  * @return {Promise} resolves if URL is valid.
  */
 module.exports.isUrl = function (url) {
-	return new Promise(function (resolve, reject) {
-		// Check if valid protocol
-		var path = url.split('/');
-		if (path[0] != 'http:' && path[0] != 'https:') {
-			reject('Invalid protocol');
-		}
+    return new Promise(function (resolve, reject) {
+        // Check if valid protocol
+        var path = url.split('/');
+        if (path[0] != 'http:' && path[0] != 'https:') {
+            reject('Invalid protocol');
+        }
 
-		// Check if URL is formatted correctly
-		if (validUrl.isUri(url)) {
-			var options = {
-				url: url,
-				headers: {
-					'User-Agent': 'request'
-				}
-			};
-			// Check if there is a response when requesting the URL
-			request.get(options, function (err, res, body) {
-				if (err) {
-					reject(err);
-				} else if (res.statusCode != 200) {
-					reject('Invalid status code <' + response.statusCode + '>');
-				} else {
-					resolve();
-				}
-			})
-		}
-	});
-} 
+        // Check if URL is formatted correctly
+        if (validUrl.isUri(url)) {
+            var options = {
+                url: url,
+                headers: {
+                    'User-Agent': 'request'
+                }
+            };
+
+            // Check if there is a response when requesting the URL
+            request.get(options, function (err, res, body) {
+                if (err) {
+                    reject(err);
+                } else if (res.statusCode != 200) {
+                    reject('Invalid status code <' + response.statusCode + '>');
+                } else {
+                    resolve();
+                }
+            })
+        }
+    });
+}
